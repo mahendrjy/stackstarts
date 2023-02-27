5b185610a836a3330494ab6902ce7dbfcedca50a
Mahendra Choudhary <mahendrjy@gmail.com>
2023-02-28 02:05:48 +0530

    Add Prose component

--- documentation/src/pages/_app.jsx
+++ documentation/src/pages/_app.jsx
@@ -1,5 +1,10 @@
+import { Prose } from '@/components/Prose'
 import '@/styles/tailwind.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Prose>
+      <Component {...pageProps} />
+    </Prose>
+  )
 }
