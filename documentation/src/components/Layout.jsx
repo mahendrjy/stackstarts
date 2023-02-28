fb70ac9987f9624d281508a98e5704137ea9fa96
Mahendra Choudhary <mahendrjy@gmail.com>
2023-03-01 03:44:50 +0530

    Add Header component

--- documentation/src/components/Layout.jsx
+++ documentation/src/components/Layout.jsx
@@ -1,8 +1,11 @@
 import { Prose } from '@/components/Prose'
+import { Header } from '@/components/Header'
 
 export function Layout({ children, title, tableOfContents }) {
   return (
     <>
+      <Header />
+
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
         <article className="py-16 px-2 xl:pr-16">
           <Prose>{children}</Prose>
