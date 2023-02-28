46d440e9f138137fd25545c50a7dc6c23cab3c8c
Mahendra Choudhary <mahendrjy@gmail.com>
2023-03-01 04:26:36 +0530

    Add Hero component

--- documentation/src/components/Layout.jsx
+++ documentation/src/components/Layout.jsx
@@ -1,13 +1,23 @@
+import { useRouter } from 'next/router'
+import { Hero } from '@/components/Hero'
 import { Prose } from '@/components/Prose'
 import { Header } from '@/components/Header'
 
-export function Layout({ children, title, tableOfContents }) {
+export function Layout({ children }) {
+  let router = useRouter()
+  let isHomePage = router.pathname === '/'
+
   return (
     <>
       <Header />
 
-      <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
-        <article className="py-16 px-2 xl:pr-16">
+      {isHomePage && <Hero />}
+
+      <div
+        id="docs"
+        className="relative mx-auto flex max-w-8xl justify-center py-16 sm:px-2 lg:px-8 xl:px-12"
+      >
+        <article className="px-2 xl:pr-16">
           <Prose>{children}</Prose>
         </article>
       </div>
